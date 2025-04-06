1d73f5756780d62b2ac25ad3d70b53590e6adb5f
ttiagosousa <ttiagopintosousa97@gmail.com>
2025-04-06 17:16:07 +0100

    Change rest api

--- src/config/urls.js
+++ src/config/urls.js
@@ -1,3 +1,7 @@
-const BASE_URL = 'http://169.254.83.107:3000/';
+// const BASE_URL = 'http://169.254.83.107:3000/';
+
+// export { BASE_URL };
+
+const BASE_URL = 'https://intri-stock-database.onrender.com/';
 
 export { BASE_URL };
