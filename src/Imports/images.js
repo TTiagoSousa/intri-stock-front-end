5246d4b5fa528c2b55486649e690e7dbe4eebebc
ttiagosousa <ttiagopintosousa97@gmail.com>
2025-05-24 15:43:53 +0100

    Update

--- src/Imports/images.js
+++ src/Imports/images.js
@@ -4,4 +4,3 @@ export { default as Microsoft_Logo } from '../Assets/Images/Companies_Logos/Micr
 export { default as Realty_Income_Logo } from '../Assets/Images/Companies_Logos/Realty_Income.png'
 export { default as Image1 } from '../Assets/Images/Auth/Image1.svg';
 export { default as Image2 } from '../Assets/Images/Auth/Image2.svg';
-export { default as Logo_IntriStock_Dark } from '../Assets/Images/Intri_Stock/Logo_IntriStock_Dark.png';
